4589c96ab337b372eb54b44b7988c854aed946ba
istiaqueahmedarik <istiaqueahmedarik@gmail.com>
2025-02-15 10:08:47 +0600

    init

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -11,6 +13,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
